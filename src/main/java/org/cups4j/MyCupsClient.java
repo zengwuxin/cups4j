eef9c32ec326f9c07313731b0027a40d9bbe580e
wuxin zeng <wuzeng@redhat.com>
2023-08-29 11:26:01 -0700

    run lp command from java app

--- src/main/java/org/cups4j/MyCupsClient.java
+++ src/main/java/org/cups4j/MyCupsClient.java
@@ -37,6 +37,24 @@ public class MyCupsClient {
             }
         });
 
+
+       ProcessBuilder processBuilder = new ProcessBuilder();
+
+       processBuilder.command("bash", "-c", "echo 'test using lp to print from java app' | lp -d hostname-printer -U java-app");
+       try {
+           Process process = processBuilder.start();
+
+           BufferedReader reader =
+                   new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+           String line;
+           while ((line = reader.readLine()) != null) {
+               System.out.println(line);
+           }
+
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
         boolean flag = true;
         while (flag) {
             Thread.sleep(2000);
